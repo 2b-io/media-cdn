8bfd9ad982e16c1ee3442a9d5b7eb50a72b2a82a
LongLH <d@dapps.me>
2017-10-01 09:20:45 +0700

    rpc using kue

--- src/server/index.js
+++ src/server/index.js
@@ -2,4 +2,18 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const kue = require('kue');
+const queue = kue.createQueue();
+
+app.get('/', (req, res, next) => {
+  const job = queue.create('media').save();
+
+  job.on('complete', data => {
+    res.json({
+      job,
+      data
+    });
+  })
+});
+
 app.listen(port, () => console.log(`Server started at ${port}`));
