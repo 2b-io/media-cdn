f22d77eb0eb2a0c282a6c858fade2696981d7272
cuongtv <truongcuong90@gmail.com>
2018-08-07 10:58:51 +0700

    - Update routes

--- src/server/endpoints/api/index.js
+++ src/server/endpoints/api/index.js
@@ -1,1 +1,12 @@
-export { default as api } from './routes'
+import bodyParser from 'body-parser'
+import express from 'express'
+
+import initRoutes from './routes'
+
+const app = express()
+
+app.use(bodyParser())
+
+initRoutes(app)
+
+export default app
--- src/server/endpoints/api/routes.js
+++ src/server/endpoints/api/routes.js
@@ -1,14 +1,7 @@
-import bodyParser from 'body-parser'
-import express from 'express'
-
 import customHeaders from './controllers/custom-headers'
 import invalidCache from './controllers/invalid-cache'
 
-const app = express()
-
-app.use(bodyParser())
-
-app.post('/cache-invalidations', invalidCache)
-app.post('/headers-custom', customHeaders)
-
-export default app
+export default (app) => {
+  app.post('/cache-invalidations', invalidCache)
+  app.post('/headers-custom', customHeaders)
+}
