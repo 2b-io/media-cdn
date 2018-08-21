67fd9f446e9532601d7246b7790ac0a945e22909
cuongtv <truongcuong90@gmail.com>
2018-08-21 09:27:27 +0700

    - fix bug and add slug to body

--- src/server/endpoints/api/controllers/upload-media.js
+++ src/server/endpoints/api/controllers/upload-media.js
@@ -1,27 +1,37 @@
 import fs from 'fs-extra'
+import mime from 'mime'
 import sh from 'shorthash'
 
+import config from 'infrastructure/config'
 import serializeError from 'serialize-error'
-
 import cache from 'services/cache'
 
+const { server: { base } } = config
+
 export default async (req, res) => {
+
+  const { slug } = req.body
+  const { mimetype, filename, path, originalname } = req.file
+  const key = sh.unique(filename)
+  const origin = `${ slug }/${ key }`
+  const ext = mime.getExtension(mimetype)
+
   try {
     const file = {
-      contentType: req.file.mimetype,
-      path: req.file.path
+      contentType: mimetype,
+      path
     }
 
-    const key = sh.unique(req.file.filename)
-
-    await cache.put(key, file, {
+    await cache.put(origin, file, {
       meta: {
-        'origin-url': req.file.originalname
+        'origin-url': `${ slug }/${ originalname }.${ ext }`
       }
     })
-    res.status(201).json({ succeed: true })
 
-    await fs.remove(file.path)
+    res.status(201).json(`${ base }/s/${ origin }.${ ext }`)
+
+    await fs.remove(path)
+
   } catch (e) {
     res.status(500).json(serializeError(e))
   }
