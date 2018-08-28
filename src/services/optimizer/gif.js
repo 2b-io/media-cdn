447c190c91706890d0d7a605ba7d291b16993e3e
cuongtv <truongcuong90@gmail.com>
2018-08-28 18:58:38 +0700

    - draf gif

--- src/services/optimizer/gif.js
+++ src/services/optimizer/gif.js
@@ -3,8 +3,25 @@ import fs from 'fs-extra'
 import gifsicle from 'gifsicle'
 import path from 'path'
 import pify from 'pify'
+import sizeOf from 'image-size'
 import uuid from 'uuid'
 import localpath from 'services/localpath'
+
+const additionalParams = ({ originWidth = 0, originHeight = 0, mode, width = 'auto', height = 'auto' }) => {
+  if (mode === 'contain') {
+    return [
+      '--resize-fit', `${ height }x${ width }`,
+    ]
+  }
+  if (mode === 'crop') {
+
+    return [
+      '--crop', `${ (originWidth-originHeight)/2 },0+${ width }x${ height }`
+    ]
+  }
+
+  return []
+}
 
 const optimizeGif = async (input, output, args) => {
 
@@ -14,30 +31,51 @@ const optimizeGif = async (input, output, args) => {
 
   const outputGif = path.join(dir, uuid.v4())
 
-  let params = [
-    '-i', input,
-    '-o', outputGif,
+  const params = [
+    // '--crop', '10,10+100x100',
+
+    // '--resize', '120x120',
+    // '--resize-fit', '0x120'
     optimize,
+    '-i', input,
+    '-o', outputGif
   ]
-  if (mode) {
-    switch (mode) {
-      case 'contain':
-        params = params.concat([
-          '--resize-height', `${ height }`,
-          '--resize-width', `${ width }`
-        ])
-        break
+
+  const originSize = sizeOf(input)
+  const { width : { originWidth }, height: { originHeight } } = originSize
+
+  if (originWidth > originHeight) {
+    if (originWidth > width || originHeight < height) {
+      //  --resize-height  = height
+      // output1
+      // --crop (originWidth-output1 - width)/2 , 0 +  widthxheight
+    }
+    if (originWidth < width || originHeight > height) {
+      //  --resize-width  = width
+      // output1
+      // --crop 0, (originHeight-output1 - height)/2 +  widthxheight
     }
   }
-  const optimizedGif = await pify(execFile)(gifsicle, params)
-    .then( (err) => {
-      if (err) {
-        return err
-      }
-      return outputGif
-    })
+  if (originWidth < originHeight) {
+    if (originHeight > height || originWidth < width ) {
+      //  --resize-width  = width
+      // output1
+      // --crop 0, (originHeight-output1 - height)/2 +  widthxheight
+    }
+    if (originWidth > width || originHeight < height ) {
+      //  --resize-height  = height
+      // output1
+      // --crop (originWidth-output1 - width)/2 , 0 +  widthxheight
+    }
+  }
+
+  await pify(execFile)(gifsicle, [
+    ...additionalParams({ originWidth, originHeight, mode, width, height }),
+    ...params
+  ])
+
   await fs.remove(output)
-  await fs.move(optimizedGif, output)
+  await fs.move(outputGif, output)
 }
 
 export default async (file, args) => {
