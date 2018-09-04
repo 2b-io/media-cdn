89642322b0146f8e74f7e35c1f2202e945193da7
cuongtv <truongcuong90@gmail.com>
2018-09-04 18:09:17 +0700

    - fix bug not delete all file in s3 and CloudFront

--- src/server/endpoints/api/controllers/delete-media.js
+++ src/server/endpoints/api/controllers/delete-media.js
@@ -1,13 +1,14 @@
 import serializeError from 'serialize-error'
 
 import config from 'infrastructure/config'
-import { deleteObject } from 'services/media'
+import cache from 'services/cache'
 
 export default async (req, res) => {
   const { slug, id } = req.params
 
   try {
-    await deleteObject(`${ config.version }/${ slug }/${ id }`)
+    await cache.deleteAll(`${ config.version }/${ slug }/${ id }`)
+    await cache.invalid([ `/u/${ slug }/${ id }`, `/p/${ slug }/${ id }` ])
     res.status(204).json({})
   } catch (e) {
     res.status(500).json(serializeError(e))
