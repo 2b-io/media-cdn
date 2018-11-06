c9df49e76cfe23fff706a20893d338464fb8ce61
cuongtv <truongcuong90@gmail.com>
2018-11-06 15:58:45 +0700

    - fix bug get value from cloud-watch equal zero

--- src/services/cloud-watch.js
+++ src/services/cloud-watch.js
@@ -1,3 +1,5 @@
+import ms from 'ms'
+
 import cloudWatch from 'infrastructure/cloud-watch'
 
 const METRIC_NAME = {
@@ -9,27 +11,57 @@ const METRIC_LABEL = {
   BytesDownloaded: 'BYTES_DOWNLOADED',
   Requests: 'REQUESTS'
 }
+// TODO: validate time
+const generateRangeTimes = (startTime, endTime, period) => {
+  const firstDate = new Date(Number(startTime))
+  const secondDate = new Date(Number(endTime))
+  let dates = []
 
-const formatResponseData = async (responseData) => {
-  return await {
-    name: METRIC_LABEL[ responseData.Label ],
-    datapoints: responseData.Datapoints.map(({
-      Timestamp,
-      Sum,
-      Average
-    }) => {
-      if (responseData.Label === 'Requests') {
-        return {
-          value: Sum ? Sum : Average,
-          timestamp: Date.parse(Timestamp)
-        }
-      } else {
-        return {
-          value: Sum,
-          timestamp: Date.parse(Timestamp)
-        }
-      }
+  if (period === ms('1d') / 1000) {
+    while (firstDate < secondDate) {
+      firstDate.setDate(firstDate.getDate() + 1)
+      dates.push(firstDate.getTime())
+    }
+    return dates
+  }
+
+  if (period === ms('1h') / 1000) {
+    while (firstDate < secondDate) {
+      firstDate.setHours(firstDate.getHours() + 1)
+      dates.push(firstDate.getTime())
+    }
+    return dates
+  }
+
+  return []
+}
+
+const formatResponseData = (responseData, { startTime, endTime, period }) => {
+  const originRangeTimes = generateRangeTimes(startTime, endTime, Number(period))
+
+  const datapoints = originRangeTimes.map((time) => ({
+    timestamp: time,
+    value: 0
+  }))
+
+  const indices = responseData.Datapoints.reduce(
+    (indices, datapoint) => ({
+      ...indices,
+      [ datapoint.Timestamp.getTime() ]: datapoint.Sum
+    }),
+    {}
+  )
+
+  const mergedDatapoints = datapoints.map(
+    (datapoint) => ({
+      ...datapoint,
+      value: indices[ datapoint.timestamp ] || 0
     })
+  )
+
+  return {
+    name: METRIC_LABEL[ responseData.Label ],
+    datapoints: mergedDatapoints
   }
 }
 
@@ -49,7 +81,7 @@ const formatRequestParams = ({
   Dimensions: [
     {
       Name: 'DistributionId',
-      Value: distributionIdentifier
+      Value: 'distributionIdentifier'
     },
     {
       Name: 'Region',
@@ -60,11 +92,12 @@ const formatRequestParams = ({
 })
 
 const getMetric = async (params) => {
+  const { startTime, endTime, period } = params
   const responseData = await cloudWatch.getMetricStatistics(
     formatRequestParams(params)
   ).promise()
 
-  return await formatResponseData(responseData)
+  return await formatResponseData(responseData, { startTime, endTime, period })
 }
 
 export default {
