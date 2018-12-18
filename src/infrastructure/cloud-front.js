6b392f5ea8fc1948f9faad5ab6ec0aecf85d2052
quangtv-ntq <quang.trinh@ntq-solution.com.vn>
2018-12-18 14:58:35 +0700

    remove clound front, clound watch, domain, route 53 & elastic search, invalidate cache

--- src/infrastructure/cloud-front.js
+++
@@ -1,9 +0,0 @@
-import aws from 'aws-sdk'
-
-import config from 'infrastructure/config'
-
-export default new aws.CloudFront({
-  region: config.aws.cloudFront.region,
-  accessKeyId: config.aws.cloudFront.accessKeyId,
-  secretAccessKey: config.aws.cloudFront.secretAccessKey
-})
--- src/server/endpoints/api/routes.js
+++ src/server/endpoints/api/routes.js
@@ -1,7 +1,3 @@
-import updateDistribution from './controllers/cloudfront/update-distribution'
-import cloudWatch from './controllers/cloud-watch'
-import invalidatePresetCache from './controllers/invalidate-preset-cache'
-import invalidateProjectCache from './controllers/invalidate-project-cache'
 import uploadMedia from './controllers/upload-media'
 import getListMedia from './controllers/get-list-media'
 import deleteMedia from './controllers/delete-media'
@@ -9,9 +5,6 @@ import getMedia from './controllers/get-media'
 import handleUpload from 'server/middlewares/handle-upload'
 
 export default (app) => {
-  app.post('/projects/:projectIdentifier/cache-invalidations', invalidateProjectCache)
-  app.post('/projects/:projectIdentifier/presets/:presetHash/cache-invalidations', invalidatePresetCache)
-  app.get('/distributions/:distributionIdentifier/metrics/:name/datapoints', cloudWatch.metric)
   app.post('/projects/:identifier/media', handleUpload, uploadMedia)
   app.get('/projects/:identifier/media', getListMedia)
   app.get('/projects/:identifier/media/:id', getMedia)
