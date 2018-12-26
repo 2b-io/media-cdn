1c312b6b3a885b649faae0dda57e2174fa90d057
cuongtv <truongcuong90@gmail.com>
2018-12-26 19:05:30 +0700

    - prevent cdn spams

---
+++ src/services/cache-request.js
@@ -0,0 +1,30 @@
+import ms from 'ms'
+
+const CACHE = {}
+
+export default {
+	create(type) {
+		if (!CACHE[ type ]) {
+			CACHE[ type ] = {}
+		}
+
+		const cache = CACHE[ type ]
+
+		return {
+			async get(index, cacheDuration, getValueFn) {
+				const needCache = !cache[ index ] || cache[ index ].cacheTime + ms(cacheDuration) < Date.now()
+
+				if (needCache) {
+			  		const cachedValue = await getValueFn()
+
+				  	cache[ index ] = {
+				  		value: cachedValue,
+				  		cacheTime: Date.now()
+				  	}
+				}
+
+				return cache[ index ].value
+			}
+		}
+	}
+}
