0a3973ad585b91997b74953e0558069b92d7c90e
cuongtv <truongcuong90@gmail.com>
2018-12-27 10:12:19 +0700

    - Fix style c

--- src/services/cache-request.js
+++ src/services/cache-request.js
@@ -3,28 +3,28 @@ import ms from 'ms'
 const CACHE = {}
 
 export default {
-	create(type) {
-		if (!CACHE[ type ]) {
-			CACHE[ type ] = {}
-		}
+  create(type) {
+    if (!CACHE[ type ]) {
+      CACHE[ type ] = {}
+    }
 
-		const cache = CACHE[ type ]
+    const cache = CACHE[ type ]
 
-		return {
-			async get(index, cacheDuration, getValueFn) {
-				const needCache = !cache[ index ] || cache[ index ].cacheTime + ms(cacheDuration) < Date.now()
+    return {
+      async get(index, cacheDuration, getValueFn) {
+        const needCache = !cache[ index ] || cache[ index ].cacheTime + ms(cacheDuration) < Date.now()
 
-				if (needCache) {
-			  		const cachedValue = await getValueFn()
+        if (needCache) {
+          const cachedValue = await getValueFn()
 
-				  	cache[ index ] = {
-				  		value: cachedValue,
-				  		cacheTime: Date.now()
-				  	}
-				}
+          cache[ index ] = {
+            value: cachedValue,
+            cacheTime: Date.now()
+          }
+        }
 
-				return cache[ index ].value
-			}
-		}
-	}
+        return cache[ index ].value
+      }
+    }
+  }
 }
