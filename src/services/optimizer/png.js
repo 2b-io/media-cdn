40ef36493b4491613eebf9a96901d79f1e3712ef
cuongtv <truongcuong90@gmail.com>
2019-05-08 05:22:10 -0700

    Resize PNG mode cover and contain with GM

--- src/services/optimizer/png.js
+++ src/services/optimizer/png.js
@@ -1,12 +1,38 @@
 import { execFile } from 'child_process'
 import fs from 'fs-extra'
+import gm from 'gm'
 import path from 'path'
 import pify from 'pify'
 import pngquant from 'pngquant-bin'
+import promise from 'bluebird'
 import sharp from 'sharp'
 import uuid from 'uuid'
 
 import localpath from 'services/localpath'
+
+const optimizePngByGM = async (file, resize, width = null, height = null, mode, output) => {
+  promise.promisifyAll(gm.prototype)
+
+  const { size: { width: originWidth, height: originHeight }  } = await gm(file.path).identifyAsync()
+
+  const originRatio = originWidth / originHeight
+  const ratio = width / height
+
+  if (resize) {
+    if (mode === 'cover') {
+      if (originRatio < ratio) {
+        return await gm(file.path).resize(width, null).writeAsync(output)
+      } else {
+        return await gm(file.path).resize(null, height).writeAsync(output)
+      }
+    }
+
+    if (mode === 'contain') {
+      return await gm(file.path).resize(width, height).writeAsync(output)
+    }
+  }
+}
+
 
 const optimizePNG = async (input, output, args) => {
   const dir = path.join(path.dirname(output), 'png')
@@ -34,7 +60,7 @@ const optimizePNG = async (input, output, args) => {
   await fs.move(outputPng, output)
 }
 
-export default async (file, args, parameters = {}) => {
+export default async (file, args, parameters = {}, optimizeByGm) => {
   const output = await localpath(file.ext)
 
   // arguments for resizing
@@ -50,6 +76,16 @@ export default async (file, args, parameters = {}) => {
   const speed = parseInt(parameters.speed, 10)
 
   const resize = !(width === 'auto' && height === 'auto')
+
+  if (optimizeByGm) {
+    await optimizePngByGM(file, resize, width, height, mode, output)
+
+    return {
+      contentType: file.contentType,
+      ext: file.ext,
+      path: output
+    }
+  }
 
   if (resize) {
     const image = sharp(file.path)
@@ -70,6 +106,12 @@ export default async (file, args, parameters = {}) => {
     await image.toFile(output)
   }
 
+  // if (true) {
+  //   promise.promisifyAll(gm.prototype)
+  //   const data = await gm(file.path).identifyAsync()
+  // }
+  //
+  // console.log('data', data)
   await optimizePNG(resize ? output : file.path, output, [
     '--quality', `${ minQuality }-${ maxQuality }`,
     '--speed', speed
